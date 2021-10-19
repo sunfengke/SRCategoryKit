fbf20e7da2447882f690f5376e1b6165f41062eb
sunfengke <sunfengke@huawei.com>
2021-10-19 15:30:26 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-SRCategoryKit_Example/Pods-SRCategoryKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SRCategoryKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SRCategoryKit_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-SRCategoryKit_Tests/Pods-SRCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SRCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SRCategoryKit_Tests
+@end
